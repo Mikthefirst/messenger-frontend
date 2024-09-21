880b9d94b890dac1667bd800c3ac04a899c60f5d
Mikhail <mikthefirst11@gmail.com>
2024-09-21 12:12:58 +0300

    Added users table and fixed chat

--- client/src/pages/chatPage/Chat.js
+++ client/src/pages/chatPage/Chat.js
@@ -1,9 +1,12 @@
 import styles from './chat.module.css';
 import Messages from './messages';
 import SendMessage from './SendMessage';
+import RoomAndUsers from './Users-in-Room';
 function Chat({ username, room, socket }) {
+    console.log(`username: ${username}, room: ${room}`);
     return (
         <div className={styles.chatContainer}>
+            <RoomAndUsers socket={socket} username={username} room={room} />
             <div>
                 <Messages socket={socket} />
                 <SendMessage socket={socket} username={username} room={room} />
