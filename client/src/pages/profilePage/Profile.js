e042ca4e5ab7d4bb0c55d363708122227cae724c
Mikhail <mikthefirst11@gmail.com>
2024-10-21 13:48:23 +0300

    Added functionality with avatars(Profile.js)

--- client/src/pages/profilePage/Profile.js
+++ client/src/pages/profilePage/Profile.js
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react';
 import styles from './profile.module.css';
-//import AvatarIcon from './../../assets/avatarIcon.png';
+import addStyles from './addimage.module.css'
+import setStyles from './setimage.module.css'
 
+//import AvatarIcon from './../../assets/avatarIcon.png';
+let username, password, token;
 
 function Profile() {
     const [imagePath, setImagePath] = useState('');
-    let username, password, token;
+
     useEffect(() => {
         if (!token) {
             token = document.cookie.split(';')[3].split('=')[1];
@@ -53,9 +56,9 @@ function Profile() {
 
 function AddImage() {
     return (
-        <form className={styles.custom__form} style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
+        <form className={addStyles.custom__form} style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
             <h1>Add image:</h1>
-            <div className={styles.custom__image_container}>
+            <div className={addStyles.custom__image_container}>
                 <label id="add-img-label" htmlFor="add-single-img">+</label>
                 <input type="file" id="add-single-img" accept="image/jpeg" />
             </div>
@@ -67,34 +70,57 @@ function AddImage() {
                 multiple
             />
             <br />
-            <button type="submit" className={styles.custom__form}>Submit</button>
+            <button type="submit" className={addStyles.custom__form}>Submit</button>
         </form>
     );
 }
 
 function ProfileImage({ imagePath }) {
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleFileChange = (event) => {
+        setSelectedFile(event.target.files[0]);
+    };
+
+    const SetImage = async (event) => {
+        event.preventDefault();
+
+        if (username && password && selectedFile) {
+            const formData = new FormData();
+            formData.append('avatar', selectedFile);
+
+            const res = await fetch(`http://localhost:3002/app/setImage?username=${username}&password=${password}`, {
+                method: 'POST',
+                body: formData
+            });
+
+            if (res.ok) {
+                alert(`New image has been added correctly.`);
+            } else {
+                alert(`Failed to add the image.`);
+            }
+        } else {
+            alert('No file selected or missing username/password.');
+        }
+    };
+
+
     return (
-        <form className={styles.custom__form} style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
+        <form className={setStyles.custom__form}>
             <h1>Your current image:</h1>
-            <img src={imagePath} alt="Profile Image" />
-
-
-            <h3>If you want to add new use this:</h3>
-
-            <div className={styles.custom__image_container} style={{ width: '60px', height: '50px' }}>
-                <label style={{ width: '60px', height: '50px' }} id="add-img-label" htmlFor="add-single-img" >+</label>
-                <input type="file" id="add-single-img" accept="image/jpeg" />
+            <div className={setStyles.custom__image_container}>
+                <img src={imagePath} alt="Profile Image" />
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <span style={{ fontSize: '70%', fontWeight: 'bold' }}>Add avatar:</span>
+                    <label htmlFor="add-single-img" id="add-img-label" >📤</label>
+                </div>
+                <input type="file" id="add-single-img" accept="image/jpeg" onChange={handleFileChange} />
             </div>
-            <input
-                type="file"
-                id="image-input"
-                name="photos"
-                accept="image/jpeg"
-                multiple
-            />
+            <input type="file" id="image-input" name="photos" accept="image/jpeg" multiple />
             <br />
-            <button type="submit" className={styles.custom__form}>Submit</button>
+            <button type="submit" className={setStyles.custom__form} onClick={SetImage}>Submit</button>
         </form>
+
     );
 }
 
